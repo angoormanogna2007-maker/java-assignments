3bf5e47490fc8e0ea597de38f46edd7b507d2ce4
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-29 19:11:42 +0530

    added square.java

---
+++ square.java
@@ -0,0 +1,15 @@
+
+    import java.util.Scanner;
+
+public class square {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter a number is : ");
+         
+        int num = scan.nextInt();
+        int square = num*num;
+        System.out.println("square of : " + num + " is : " + square);
+    }
+    
+}
+
