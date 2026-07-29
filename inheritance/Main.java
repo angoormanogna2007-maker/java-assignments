39ad1bb654461a416ca54485f9e0394ca43a1e55
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-29 11:40:51 +0530

    added person.java

--- inheritance/Main.java
+++ inheritance/Main.java
@@ -2,7 +2,7 @@ package inheritance;
 
 public class Main {
     public static void main(String[] args){
-        Student s1 = new Student("neha",19,35,"mpcs");
+        student s1 = new student("neha",19,35,"mpcs");
         s1.displayStudent();
     }
 
