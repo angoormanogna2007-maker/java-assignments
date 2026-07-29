c34b0505a52daf3d331b9379283a9dd4692a0b45
A MANOGNA <angoormanogna2007@gmail.com>
2026-07-29 10:57:29 +0530

    added Shape and Mainn

---
+++ inheritance/Mainn.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Mainn{
+    public static void main(String[] args){
+        Rectangle r1 = new Rectangle("blue",9.5,2.0);
+        r1.displayRectangle();
+    }
+    
+}
