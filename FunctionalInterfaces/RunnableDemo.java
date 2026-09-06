b5558381bb6119e2ba9abcf1f0d6c9f494f62034
A MANOGNA <angoormanogna2007@gmail.com>
2026-09-06 14:59:41 +0530

    Added RunnableDemo.java

---
+++ FunctionalInterfaces/RunnableDemo.java
@@ -0,0 +1,15 @@
+package FunctionalInterfaces;
+
+public class RunnableDemo {
+    public static void main(String[] args){
+        Runnable r1 = new Runnable(){
+            @Override
+            public void run() {
+                System.out.println("Hi from anonymous class");
+            }
+        };
+        r1.run();
+
+    }
+
+}
